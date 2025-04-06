98307e04d33e43dc84eb617071dbd96e22713e86
alikian <alikian@gmail.com>
2025-04-05 19:32:53 -0700

    fix chatbot

--- src/components/ChatbotDetails.tsx
+++ src/components/ChatbotDetails.tsx
@@ -1,29 +1,48 @@
-import React, { useEffect, useState, useRef } from 'react';
 import { useParams } from 'react-router-dom';
-import { Chatbot, Conversation, MessageResponse, Message } from '../models';
 import { DataService } from '../services/DataService';
-import { Box, Typography, TextField, Button, CircularProgress, Alert, Paper } from '@mui/material';
+import React, { useEffect, useState, useRef } from 'react';
+import { Chatbot, Conversation, MessageResponse, Message, Base } from '../models';
+import { Box, Typography, TextField, Button, CircularProgress, Alert, Paper, Select, MenuItem, FormControl, InputLabel } from '@mui/material';
 import ChatbotRunService from '../services/ChatbotRunService';
 
 const ChatbotDetails: React.FC = () => {
   const { chatbotId } = useParams<{ chatbotId: string }>();
   const [chatbot, setChatbot] = useState<Chatbot | null>(null);
+  const [updatedChatbot, setUpdatedChatbot] = useState<Partial<Chatbot>>({});
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
+  const chatbotService = new DataService<Chatbot>('chatbots');
+  const [models, setModels] = useState<string[]>([]);
   const [question, setQuestion] = useState<string>('');
   const [messages, setMessages] = useState<Message[]>([]);
   const [conversation, setConversation] = useState<Conversation | null>(null);
-  const [loading, setLoading] = useState<boolean>(true);
   const [loadingChat, setLoadingChat] = useState<boolean>(false);
-  const [error, setError] = useState<string | null>(null);
-  const chatbotService = new DataService<Chatbot>('chatbots');
   const conversationService = new DataService<Conversation>('conversations');
+  const baseService = new DataService<Base>('bases');
+  const [bases, setBases] = useState<Base[]>([]);
   const chatbotRunService = new ChatbotRunService(chatbotId!);
   const messagesEndRef = useRef<HTMLDivElement>(null);
+  const llms =
+    [{
+      'llm': 'openai',
+      'modeles': ['gpt-4o', 'gpt-4o-mini']
+    },
+    {
+      'llm': 'anthropic',
+      'modeles': ['claude-3-7-sonnet-latest', 'claude-3-5-haiku-latest']
+    }
+    ]
+    ;
+
+
 
   useEffect(() => {
     const fetchChatbot = async () => {
-      console.log('chatbotId:', chatbotId);
       try {
-        const data = await chatbotService.get(chatbotId!);
+        const bases = await baseService.getAll();
+        setBases(bases);
+
+        const chatbot = await chatbotService.get(chatbotId!);
         const newConversation: Conversation = {
           conversationId: '', // Generate or assign a unique ID
           createdAt: new Date().getTime(),
@@ -33,7 +52,13 @@ const ChatbotDetails: React.FC = () => {
         const convResult = await conversationService.create(newConversation);
         newConversation.conversationId = convResult.conversationId;
         setConversation(newConversation);
-        setChatbot(data);
+        setChatbot(chatbot);
+        setUpdatedChatbot(chatbot); // Initialize updatedChatbot with fetched data
+
+        let modelesFound = llms.find((llm) => llm.llm === chatbot.llm)?.modeles || [];
+        setModels(modelesFound);
+        handleChange('llmModel', modelesFound[0]);
+
       } catch (error) {
         console.error('Error fetching chatbot:', error);
         setError('Failed to fetch chatbot');
@@ -53,7 +78,7 @@ const ChatbotDetails: React.FC = () => {
         text: question,
         sender: 'user',
         timestamp: new Date().getTime(),
-        conversationId: conversation?.conversationId || ''
+        conversationId: conversation?.conversationId || '',
       };
       console.log('userMessage:', userMessage);
       setMessages((prevMessages) => [...prevMessages, userMessage]);
@@ -61,7 +86,12 @@ const ChatbotDetails: React.FC = () => {
       const response: MessageResponse = await chatbotRunService.postMessage(userMessage);
       console.log('response:', response.text);
 
-      const chatbotMessage: Message = { sender: 'chatbot', text: response.text, timestamp: Date.now(), conversationId: conversation?.conversationId || '' };
+      const chatbotMessage: Message = {
+        sender: 'chatbot',
+        text: response.text,
+        timestamp: Date.now(),
+        conversationId: conversation?.conversationId || '',
+      };
       setMessages((prevMessages) => [...prevMessages, chatbotMessage]);
       console.log('chatbotMessage:', chatbotMessage);
 
@@ -81,9 +111,23 @@ const ChatbotDetails: React.FC = () => {
     }
   };
 
-  useEffect(() => {
-    scrollToTop();
-  }, [messages]);
+  const handleSave = async () => {
+    try {
+      if (chatbotId && updatedChatbot) {
+        const updatedData = await chatbotService.update(chatbotId, updatedChatbot as Chatbot);
+        setChatbot(updatedData);
+        setError(null);
+      }
+    } catch (error) {
+      console.error('Error updating chatbot:', error);
+      setError('Failed to update chatbot');
+    }
+  };
+
+  const handleChange = (field: keyof Chatbot, value: string | number) => {
+    console.log('Updated field:', field, 'New value:', value);
+    setUpdatedChatbot((prev) => ({ ...prev, [field]: value }));
+  };
 
   if (loading) {
     return <CircularProgress />;
@@ -98,51 +142,179 @@ const ChatbotDetails: React.FC = () => {
   }
 
   return (
-    <Box>
-      <Typography variant="h4" gutterBottom>
-        Chatbot Details
-      </Typography>
-      <Typography variant="body1"><strong>Base ID:</strong> {chatbot.baseId}</Typography>
-      <Typography variant="body1"><strong>Name:</strong> {chatbot.chatbotName}</Typography>
-      <Typography variant="body1"><strong>Description:</strong> {chatbot.chatbotDescription}</Typography>
-      <Typography variant="body1"><strong>Created At:</strong> {new Date(chatbot.createdAt).toLocaleString()}</Typography>
-      <Typography variant="body1"><strong>aqPrompt:</strong> {chatbot.aqPrompt}</Typography>
-      <Typography variant="body1"><strong>cqPrompt:</strong> {chatbot.cqPrompt}</Typography>
-      <Typography variant="body1"><strong>LLM:</strong> {chatbot.llm}</Typography>
-      <Typography variant="body1"><strong>LLM Model:</strong> {chatbot.llmModel}</Typography>
-      <Typography variant="body1"><strong>LLM Temperature:</strong> {chatbot.llmTemperature}</Typography>
+    <>
+      <Box
+        sx={{
+          display: 'flex', // Use flexbox to arrange child boxes side by side
+          gap: 3, // Add spacing between the boxes
+          mt: 3,
+          width: '100%', // Ensure the parent box takes full width
+        }}
+      >
+        {/* Left Box: Chatbot Details */}
+        <Box
+          sx={{
+            flex: 1, // Allow the box to take equal space
+            maxWidth: '50%', // Limit the width to 50% of the parent container
+            backgroundColor: 'background.paper',
+            p: 3,
+            borderRadius: 2,
+            boxShadow: 3,
+          }}
+        >
+          <Typography variant="h4" gutterBottom>
+            Chatbot Details
+          </Typography>
+          <>
+            <TextField
+              label="Name"
+              value={updatedChatbot.chatbotName || ''}
+              onChange={(e) => handleChange('chatbotName', e.target.value)}
+              fullWidth
+              margin="normal"
+            />
+            <TextField
+              label="Description"
+              value={updatedChatbot.chatbotDescription || ''}
+              onChange={(e) => handleChange('chatbotDescription', e.target.value)}
+              fullWidth
+              margin="normal"
+            />
+            <FormControl fullWidth margin="normal" required>
+              <InputLabel id="base-select-label">Base</InputLabel>
+              <Select
+                labelId="base-select-label"
+                value={updatedChatbot.baseId || ''}
+                onChange={(e) => {
+                  handleChange('baseId', e.target.value);
+                }}
+              >
+                {bases.map((base) => (
+                  <MenuItem key={base.baseId} value={base.baseId}>
+                    {base.baseName}
+                  </MenuItem>
+                ))}
+              </Select>
+            </FormControl>
 
-      <Typography variant="body1"><strong>Conversation Id:</strong> {conversation?.conversationId}</Typography>
+            <TextField
+              label="aqPrompt"
+              value={updatedChatbot.aqPrompt || ''}
+              onChange={(e) => handleChange('aqPrompt', e.target.value)}
+              multiline
+              fullWidth
+              margin="normal"
+            />
+            <TextField
+              label="cqPrompt"
+              value={updatedChatbot.cqPrompt || ''}
+              onChange={(e) => handleChange('cqPrompt', e.target.value)}
+              multiline
+              fullWidth
+              margin="normal"
+            />
 
-      <Box mt={4}>
-        {loadingChat && <CircularProgress />}
-        {error && <Alert severity="error">{error}</Alert>}
-        <Box mt={2}  ref={messagesEndRef}>
-          <Typography variant="h6">Conversation:</Typography>
-          {messages.map((message, index) => (
-            <Paper key={index} style={{ padding: '10px', margin: '10px 0', backgroundColor: message.sender === 'user' ? '#e0f7fa' : '#f1f8e9' }}>
-              <Typography variant="body2" color="textSecondary">
-                {message.sender === 'user' ? 'You' : 'Chatbot'} at {new Date(message.timestamp).toLocaleTimeString()}
-              </Typography>
-              <Typography variant="body1">{message.text}</Typography>
-            </Paper>
-          ))}
+            <FormControl fullWidth margin="normal" required>
+              <InputLabel id="llm-select-label">LLM</InputLabel>
+              <Select
+                labelId="llm-select-label"
+                value={updatedChatbot.llm || ''}
+                onChange={(e) => {
+                  handleChange('llm', e.target.value);
+                  let modelesFound = llms.find((llm) => llm.llm === e.target.value)?.modeles || [];
+                  setModels(modelesFound);
+                  handleChange('llmModel', modelesFound[0]);
+                }}
+              >
+                {llms.map((llm) => (
+                  <MenuItem key={llm.llm} value={llm.llm}>
+                    {llm.llm}
+                  </MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+
+            <FormControl fullWidth margin="normal" required>
+              <InputLabel id="llm-model-select-label">LLM Model</InputLabel>
+              <Select
+                labelId="llm-model-select-label"
+                value={updatedChatbot.llmModel || ''}
+                onChange={(e) => {
+                  handleChange('llmModel', e.target.value);
+                }}
+              >
+                {models.map((model) => (
+                  <MenuItem key={model} value={model}>
+                    {model}
+                  </MenuItem>
+                ))}
+              </Select>
+            </FormControl>
+
+            <TextField
+              label="LLM Temperature"
+              type="number"
+              value={updatedChatbot.llmTemperature || ''}
+              onChange={(e) => handleChange('llmTemperature', parseFloat(e.target.value))}
+              fullWidth
+              margin="normal"
+            />
+            <Button variant="contained" color="primary" onClick={handleSave} sx={{ mt: 2 }}>
+              Save
+            </Button>
+          </>
+        </Box>
+
+        {/* Right Box: Conversation */}
+        <Box
+          sx={{
+            flex: 1, // Allow the box to take equal space
+            maxWidth: '50%', // Limit the width to 50% of the parent container
+            backgroundColor: 'background.paper',
+            p: 3,
+            borderRadius: 2,
+            boxShadow: 3,
+          }}
+        >
+          <Box mt={4}>
+            {loadingChat && <CircularProgress />}
+            {error && <Alert severity="error">{error}</Alert>}
+            <Box mt={2} ref={messagesEndRef}>
+              <Typography variant="h6">Conversation:</Typography>
+              {messages.map((message, index) => (
+                <Paper
+                  key={index}
+                  style={{
+                    padding: '10px',
+                    margin: '10px 0',
+                    backgroundColor: message.sender === 'user' ? '#e0f7fa' : '#f1f8e9',
+                  }}
+                >
+                  <Typography variant="body2" color="textSecondary">
+                    {message.sender === 'user' ? 'You' : 'Chatbot'} at{' '}
+                    {new Date(message.timestamp).toLocaleTimeString()}
+                  </Typography>
+                  <Typography variant="body1">{message.text}</Typography>
+                </Paper>
+              ))}
+            </Box>
+          </Box>
+          <Typography variant="h5" gutterBottom>
+            Ask a Question
+          </Typography>
+          <TextField
+            label="Question"
+            value={question}
+            onChange={(e) => setQuestion(e.target.value)}
+            fullWidth
+            margin="normal"
+          />
+          <Button variant="contained" color="primary" onClick={handleAskQuestion} disabled={loadingChat}>
+            Ask
+          </Button>
         </Box>
       </Box>
-      <Typography variant="h5" gutterBottom>
-          Ask a Question
-        </Typography>
-        <TextField
-          label="Question"
-          value={question}
-          onChange={(e) => setQuestion(e.target.value)}
-          fullWidth
-          margin="normal"
-        />
-        <Button variant="contained" color="primary" onClick={handleAskQuestion} disabled={loadingChat}>
-          Ask
-        </Button>
-    </Box>
+    </>
   );
 };
 
